d87f73b2efd6a6b653a27ea3b1f64281fde0cf06
Whisky3000 <daison3000@gmail.com>
2011-11-07 02:05:19 +0700

    chi moi den tracing logging, do nhom con gap nhieu truc trac nen chua hoan thanh Syn and Asyn

---
+++ Connect/ClientForm/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txbWsHttp = new System.Windows.Forms.TextBox();
+            this.btnWsHttp = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbNetTcp = new System.Windows.Forms.TextBox();
+            this.btnNetTcp = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbBasicHttp = new System.Windows.Forms.TextBox();
+            this.btnBasicHttp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txbAdd = new System.Windows.Forms.TextBox();
+            this.txbAll = new System.Windows.Forms.TextBox();
+            this.btnSyn = new System.Windows.Forms.Button();
+            this.btnAsyn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAsyn);
+            this.groupBox1.Controls.Add(this.btnSyn);
+            this.groupBox1.Controls.Add(this.txbAll);
+            this.groupBox1.Controls.Add(this.txbAdd);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txbWsHttp);
+            this.groupBox1.Controls.Add(this.btnWsHttp);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txbNetTcp);
+            this.groupBox1.Controls.Add(this.btnNetTcp);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txbBasicHttp);
+            this.groupBox1.Controls.Add(this.btnBasicHttp);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(558, 353);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // txbWsHttp
+            // 
+            this.txbWsHttp.Location = new System.Drawing.Point(107, 168);
+            this.txbWsHttp.Name = "txbWsHttp";
+            this.txbWsHttp.Size = new System.Drawing.Size(283, 20);
+            this.txbWsHttp.TabIndex = 8;
+            // 
+            // btnWsHttp
+            // 
+            this.btnWsHttp.Location = new System.Drawing.Point(6, 168);
+            this.btnWsHttp.Name = "btnWsHttp";
+            this.btnWsHttp.Size = new System.Drawing.Size(75, 23);
+            this.btnWsHttp.TabIndex = 7;
+            this.btnWsHttp.Text = "Get Author";
+            this.btnWsHttp.UseVisualStyleBackColor = true;
+            this.btnWsHttp.Click += new System.EventHandler(this.btnWsHttp_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "WsHttpBinding";
+            // 
+            // txbNetTcp
+            // 
+            this.txbNetTcp.Location = new System.Drawing.Point(107, 112);
+            this.txbNetTcp.Name = "txbNetTcp";
+            this.txbNetTcp.Size = new System.Drawing.Size(283, 20);
+            this.txbNetTcp.TabIndex = 5;
+            // 
+            // btnNetTcp
+            // 
+            this.btnNetTcp.Location = new System.Drawing.Point(6, 112);
+            this.btnNetTcp.Name = "btnNetTcp";
+            this.btnNetTcp.Size = new System.Drawing.Size(75, 23);
+            this.btnNetTcp.TabIndex = 4;
+            this.btnNetTcp.Text = "Get Author";
+            this.btnNetTcp.UseVisualStyleBackColor = true;
+            this.btnNetTcp.Click += new System.EventHandler(this.btnNetTcp_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NetTcpBinding";
+            // 
+            // txbBasicHttp
+            // 
+            this.txbBasicHttp.Location = new System.Drawing.Point(107, 55);
+            this.txbBasicHttp.Name = "txbBasicHttp";
+            this.txbBasicHttp.Size = new System.Drawing.Size(283, 20);
+            this.txbBasicHttp.TabIndex = 2;
+            // 
+            // btnBasicHttp
+            // 
+            this.btnBasicHttp.Location = new System.Drawing.Point(6, 55);
+            this.btnBasicHttp.Name = "btnBasicHttp";
+            this.btnBasicHttp.Size = new System.Drawing.Size(75, 23);
+            this.btnBasicHttp.TabIndex = 1;
+            this.btnBasicHttp.Text = "Get Author";
+            this.btnBasicHttp.UseVisualStyleBackColor = true;
+            this.btnBasicHttp.Click += new System.EventHandler(this.btnBasicHttp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BasicHttpBinding";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(204, 237);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // txbAdd
+            // 
+            this.txbAdd.Location = new System.Drawing.Point(107, 211);
+            this.txbAdd.Name = "txbAdd";
+            this.txbAdd.Size = new System.Drawing.Size(283, 20);
+            this.txbAdd.TabIndex = 10;
+            // 
+            // txbAll
+            // 
+            this.txbAll.Location = new System.Drawing.Point(107, 266);
+            this.txbAll.Name = "txbAll";
+            this.txbAll.Size = new System.Drawing.Size(283, 20);
+            this.txbAll.TabIndex = 11;
+            // 
+            // btnSyn
+            // 
+            this.btnSyn.Location = new System.Drawing.Point(151, 307);
+            this.btnSyn.Name = "btnSyn";
+            this.btnSyn.Size = new System.Drawing.Size(75, 23);
+            this.btnSyn.TabIndex = 12;
+            this.btnSyn.Text = "Syn";
+            this.btnSyn.UseVisualStyleBackColor = true;
+            this.btnSyn.Click += new System.EventHandler(this.btnSyn_Click);
+            // 
+            // btnAsyn
+            // 
+            this.btnAsyn.Location = new System.Drawing.Point(269, 307);
+            this.btnAsyn.Name = "btnAsyn";
+            this.btnAsyn.Size = new System.Drawing.Size(75, 23);
+            this.btnAsyn.TabIndex = 13;
+            this.btnAsyn.Text = "Asyn";
+            this.btnAsyn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 378);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txbWsHttp;
+        private System.Windows.Forms.Button btnWsHttp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbNetTcp;
+        private System.Windows.Forms.Button btnNetTcp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbBasicHttp;
+        private System.Windows.Forms.Button btnBasicHttp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAsyn;
+        private System.Windows.Forms.Button btnSyn;
+        private System.Windows.Forms.TextBox txbAll;
+        private System.Windows.Forms.TextBox txbAdd;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Connect/ClientForm/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using ClientForm.ClientServiceReference;
+
+namespace ClientForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBasicHttp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EndpointAddress address = new EndpointAddress("http://localhost:2000/Connect");
+                BasicHttpBinding binding = new BasicHttpBinding();
+                //khai bao cong ket noi
+                IService proxy = ChannelFactory<IService>.CreateChannel(binding, address);
+                string groupBasicHttp = proxy.GetAuthor();
+                txbBasicHttp.Text = groupBasicHttp;
+            }
+            catch
+            {
+                MessageBox.Show("Link is error! Try again!","Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnNetTcp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EndpointAddress address = new EndpointAddress("net.tcp://localhost:3000/Connect");
+                NetTcpBinding binding = new NetTcpBinding();
+
+                IService proxy = ChannelFactory<IService>.CreateChannel(binding, address);
+                string groupTcp = proxy.GetAuthor();
+                txbNetTcp.Text = groupTcp;
+            }
+            catch
+            {
+                MessageBox.Show("Link is error!Try again!", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnWsHttp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EndpointAddress address = new EndpointAddress("http://localhost:8732/Design_Time_Addresses/Host/Service1/");
+                WSHttpBinding binding = new WSHttpBinding();
+
+                IService proxy = ChannelFactory<IService>.CreateChannel(binding, address);
+                string groupWsHttp = proxy.GetAuthor();
+                txbWsHttp.Text = groupWsHttp;
+            }
+            catch
+            {
+                MessageBox.Show("Link is error! Try a again!", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        
+        
+    }
+}
